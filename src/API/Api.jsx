8bb964219fb8628254dff73924565367ad077928
Eunice Abieyuwa Igbinedion <eunice.igbinedion@yahoo.com>
2024-08-13 07:22:52 +0200

    Update

--- src/API/Api.jsx
+++ src/API/Api.jsx
@@ -5,6 +5,7 @@ const API_URL = 'https://bible-api.com/';
 export const getVerse = async (verse) => {
     try {
         const response = await axios.get(`${API_URL}${verse}`);
+        console.log(response);
         return response.data;
     } catch (error) {
         console.error('Error fetching verse:', error);
